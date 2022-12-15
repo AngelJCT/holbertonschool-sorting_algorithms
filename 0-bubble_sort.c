b47456493d942f8ec6fb94ea70283e4ce82fb489
Parallels <acarrion_pr@yahoo.com>
2022-12-14 20:18:03 -0400

    fixing algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,21 +6,18 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp, flag;
+	unsigned int i, temp, flag;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
-		for (j = 0; j < size - 1 - i; i++)
+		if (array[i] > array[i + 1])
 		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-				flag = 1;
-			}
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			print_array(array, size);
+			flag = 1;
 		}
 		if (flag == 0)
 			break;
